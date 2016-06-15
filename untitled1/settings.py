a24946d3207f4bd4a74cebdcdbc0ce20c8698270
Stein Lobo <stein@ebb502301.ebb.gatech.edu>
2016-06-15 12:26:25 -0400

    Meeting draft 6/15

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'dsz^29o8b$jf15^)#d_4k$sxtf=)sido+gexz8+zvs^)dneh#-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
